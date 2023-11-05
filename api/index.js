169735552cb46e553575d4248ae800b71c587124
princetomar <madm47157@gmail.com>
2023-11-05 07:05:27 +0530

    Database Connected

--- api/index.js
+++ api/index.js
@@ -1,4 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+// initialize dotenv
+dotenv.config();
+
+// CONNECT MONGODB DATABASE TO OUR PROJECT
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then((val) => {
+    console.log("connected to db");
+  })
+  .catch((err) => {
+    console.log(`Getting error while connecting to db - ${err}`);
+  });
+
 const app = express();
 
 app.listen(3000, () => {
